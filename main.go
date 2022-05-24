91e9062079775ee1e0349c62b84c780592519e98
William Alan Patterson <39136284+walanpat@users.noreply.github.com>
2022-05-24 15:31:12 -0400

    Ping Pong
    Ping pong response functionality enabled

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"goland-discord-bot/bot"
+	"goland-discord-bot/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
