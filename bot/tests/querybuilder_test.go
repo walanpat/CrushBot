3b8cf5c38e1a4d13b43214af38c9f1fecc79e4a7
William Alan Patterson <walanpatterson@gmail.com>
2022-08-16 17:20:29 -0400

    Added more tests

--- bot/tests/querybuilder_test.go
+++ bot/tests/querybuilder_test.go
@@ -15,6 +15,23 @@ type TestCase struct {
 	actual   bool
 }
 
+//t.Run("", func(t *testing.T) {
+//	testCase := TestCase{
+//		input:    "",
+//		expected: "https://api.scryfall.com/cards/search?q=",
+//	}
+//	output, err := builder.MtgQueryBuilder(testCase.input)
+//	if testCase.expected != output {
+//		fmt.Println("Output:   ", output)
+//		fmt.Println("Expected: ", testCase.expected)
+//		t.Fail()
+//	}
+//	if err != nil {
+//		t.Fail()
+//	}
+//})
+
+//Testing color inputs
 func TestColorsQueryBuilder(t *testing.T) {
 
 	//Color testing
@@ -136,7 +153,7 @@ func TestColorsQueryBuilder(t *testing.T) {
 
 //Tests for Power, Toughness, Loyalty, and CMC
 func TestInequalitiesQueryBuilder(t *testing.T) {
-	t.Run("1 sided inequality test toughness", func(t *testing.T) {
+	t.Run("1 sided inequality toughness", func(t *testing.T) {
 		testCase := TestCase{
 			input:    "type:vampire, toughness:<4, color:bu",
 			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+tou%3C4+",
@@ -152,24 +169,7 @@ func TestInequalitiesQueryBuilder(t *testing.T) {
 			t.Fail()
 		}
 	})
-	t.Run("2 sided inequality test cmc", func(t *testing.T) {
-		testCase := TestCase{
-			input:    "cmc:3<=m<6",
-			expected: "https://api.scryfall.com/cards/search?q=cmc%3E%3D3+cmc%3C6+",
-			//expected: "https://api.scryfall.com/cards/search?q=",
-		}
-		output, err := builder.MtgQueryBuilder(testCase.input)
-		if testCase.expected != output {
-			fmt.Println("Output:   ", output)
-			fmt.Println("Expected: ", testCase.expected)
-			t.Fail()
-		}
-		if err != nil {
-			t.Fail()
-		}
-	})
-
-	t.Run("test 2 sided query toughness", func(t *testing.T) {
+	t.Run("2 sided inequality toughness", func(t *testing.T) {
 		testCase := TestCase{
 			input:    "type:vampire, toughness:4<=t<6, color:bu",
 			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+tou%3E%3D4+tou%3C6+",
@@ -185,19 +185,171 @@ func TestInequalitiesQueryBuilder(t *testing.T) {
 		}
 	})
 
+	t.Run("1 sided inequality cmc", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:vampire, cmc:<=4, color:bu",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+cmc%3C=4\n",
+			//expected: "https://api.scryfall.com/cards/search?q=",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("1 sided inequality cmc <=10", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:vampire, cmc:<=4, color:bu",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+cmc%3C=4\n",
+			//expected: "https://api.scryfall.com/cards/search?q=",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("2 sided inequality cmc", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "cmc:3<=m<6",
+			expected: "https://api.scryfall.com/cards/search?q=cmc%3E%3D3+cmc%3C6+",
+			//expected: "https://api.scryfall.com/cards/search?q=",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("1 sided inequality power", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:vampire, power:<=2, cmc:<=4, color:bu",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+cmc%3C=4pow%3C=2",
+			//expected: "https://api.scryfall.com/cards/search?q=",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("2 sided inequality power", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:vampire, power:2<=p<=3, cmc:<=4, color:bu",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Avampire+c%3Dbu+cmc%3C=4pow%3E%3D2+pow%3C%3D3+",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
 }
 
+//testing type inputs
 func TestTypesQueryBuilder(t *testing.T) {
+	t.Run("testing for  goblin sorcery", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:goblin sorcery",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Agoblin+t%3Asorcery+",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("testing for fae instant", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:fae instant",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Afae+t%3Ainstant+",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("testing Orc Pirate Wizard", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:orc pirate wizard",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Aorc+t%3Apirate+t%3Awizard+",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("testing for Zombie Minotaur Warrior", func(t *testing.T) {
+		testCase := TestCase{
+			input:    "type:minotaur zombie warrior",
+			expected: "https://api.scryfall.com/cards/search?q=t%3Aminotaur+t%3Azombie+t%3Awarrior+",
+		}
+		output, err := builder.MtgQueryBuilder(testCase.input)
+		if testCase.expected != output {
+			fmt.Println("Output:   ", output)
+			fmt.Println("Expected: ", testCase.expected)
+			t.Fail()
+		}
+		if err != nil {
+			t.Fail()
+		}
+	})
 }
+
+//testing is inputs
 func TestIsQueryBuilder(t *testing.T) {
 }
+
+//testing functional tag inputs
 func TestFunctionQueryBuilder(t *testing.T) {
 }
+
+//testing art tag inputs
 func TestArtQueryBuilder(t *testing.T) {
 }
+
+//testing rarity inputs
 func TestRarityQueryBuilder(t *testing.T) {
 }
+
+//testing text search input
 func TestTextQueryBuilder(t *testing.T) {
 }
+
+//testing query builder return statement
 func TestQueryBuilder(t *testing.T) {
 }
