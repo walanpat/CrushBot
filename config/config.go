fc2624115e4a14dbba094f67bb7738b995bd522f
William Alan Patterson <walanpatterson@gmail.com>
2024-07-18 16:34:47 -0400

    Committing small changes

--- config/config.go
+++ config/config.go
@@ -19,7 +19,7 @@ type configStruct struct {
 
 func ReadConfig() error {
 	fmt.Println("Reading config file...")
-	file, err := ioutil.ReadFile("./config/config.json") // ioutil package's ReadFile method which we read config.json and return it's value we will then store it in file variable and if an error ocurrs it will be stored in err .
+	file, err := ioutil.ReadFile("./config/config.json") // ioutil package's ReadFile method which we read config.json and return its value we will then store it in file variable and if an error ocurrs it will be stored in err .
 	// Handling error and printing it using fmt package's Println function and returning it .
 	if err != nil {
 		fmt.Println(err.Error())
